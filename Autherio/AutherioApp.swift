63d3684b4cd4302d8bf5f39a7cd89201957176ce
Kamil Kozioł <koziol.kamil@protonmail.com>
2022-02-15 10:57:43 +0100

    Initial Commit

---
+++ Autherio/AutherioApp.swift
@@ -0,0 +1,17 @@
+//
+//  AutherioApp.swift
+//  Autherio
+//
+//  Created by Kamil Kozioł on 15/02/2022.
+//
+
+import SwiftUI
+
+@main
+struct AutherioApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
